377eae6ab9149e5c0edc6d760dbe6caca1317411
Will Craftia <willcraftia@gmail.com>
2012-02-15 12:24:25 +0900

    fixed memory leak.

--- Framework.UI/Animations/PropertyLerpAnimation.cs
+++ Framework.UI/Animations/PropertyLerpAnimation.cs
@@ -14,23 +14,32 @@ namespace Willcraftia.Xna.Framework.UI.Animations
     public class PropertyLerpAnimation : LerpAnimation
     {
         /// <summary>
-        /// 対象とするオブジェクトを取得または設定します。
+        /// 対象とする Control への弱参照。
         /// </summary>
-        public object Target { get; set; }
-
-        /// <summary>
-        /// 対象とするプロパティの名前を取得または設定します。
-        /// </summary>
-        public string PropertyName { get; set; }
+        WeakReference targetReference = new WeakReference(null);
 
         /// <summary>
         /// 対象とするプロパティ。
         /// </summary>
         PropertyInfo property;
 
+        /// <summary>
+        /// 対象とするオブジェクトを取得または設定します。
+        /// </summary>
+        public object Target
+        {
+            get { return targetReference.Target; }
+            set { targetReference.Target = value; }
+        }
+
+        /// <summary>
+        /// 対象とするプロパティの名前を取得または設定します。
+        /// </summary>
+        public string PropertyName { get; set; }
+
         protected override void Update(GameTime gameTime, TimeSpan playElapsedTime, float current)
         {
-            if (Target == null) throw new InvalidOperationException("Target is null.");
+            if (targetReference.Target == null) return;
 
             // プロパティを取得します。
             if (property == null) property = Target.GetType().GetProperty(PropertyName);
